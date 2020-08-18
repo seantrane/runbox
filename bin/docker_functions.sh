ff3469fdf5e335343ac37632f8f5790a1b467308
Sean Trane Sciarrone <seantrane@users.noreply.github.com>
2020-08-18 06:33:50 -0400

    fix: refactor docker functions
    - Load .env from same directory as Dockerfiles
    - Set docker options using arrays
    - Add config for docker container name and hostname

--- bin/docker_functions.sh
+++ bin/docker_functions.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-__runbox_repo_path="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-
 #######################################
 # Build Docker image.
 # Globals:
@@ -23,28 +21,28 @@ build_docker_image () {
 
   docker rmi -f "$__docker_image_tag" 2> /dev/null
   # Set options for 'docker build' command.
-  _docker_opts=()
+  __docker_opts=()
   # Do not use cache when building the image.
-  # _docker_opts+=(--no-cache)
+  # __docker_opts+=(--no-cache)
   # Remove intermediate containers after a successful build.
-  _docker_opts+=(--rm)
+  __docker_opts+=(--rm)
   # Name of the Dockerfile (Default is 'PATH/Dockerfile').
-  _docker_opts+=(-f "${__docker_config_file:-$__docker_context_path/Dockerfile}")
+  __docker_opts+=(-f "${__docker_config_file:-$__docker_context_path/Dockerfile}")
   # Name and optionally a tag in the 'name:tag' format.
-  _docker_opts+=(-t "$__docker_image_tag")
+  __docker_opts+=(-t "$__docker_image_tag")
 
   # Start timer to measure/report on docker build time.
-  _start_time="$(date +%s)"
+  __start_time="$(date +%s)"
 
-  docker build "${_docker_opts[@]}" "$@" "$__docker_context_path"
+  docker build "${__docker_opts[@]}" "$@" "$__docker_context_path"
 
-  _end_time="$(date +%s)"
-  _runlen="$(echo "$_end_time - ${_start_time}" | bc)"
-  _runtime="$_runlen seconds"
+  __end_time="$(date +%s)"
+  __runlen="$(echo "$__end_time - ${__start_time}" | bc)"
+  __runtime="$__runlen seconds"
   if type "python" &> /dev/null; then
-    _runtime="$(python -c "print '%u:%02u' % ($_runlen/60, $_runlen%60)")"
+    __runtime="$(python -c "print '%u:%02u' % ($__runlen/60, $__runlen%60)")"
   fi
-  echo -e "\n$(date +%Y-%m-%dT%H:%M:%S%z) [INFO] Docker image ($__docker_image_tag) build took ${_runtime}.\n"
+  echo -e "\n$(date +%Y-%m-%dT%H:%M:%S%z) [INFO] Docker image ($__docker_image_tag) build took ${__runtime}.\n"
 
   # shellcheck disable=SC2046
   docker rmi -f $(docker images -f "dangling=true" -q) 2> /dev/null
@@ -92,7 +90,8 @@ run_docker_container () {
   __pwd_path="$(pwd)"
   __docker_image_tag="${1:-this/box:latest}" && shift
   __docker_context_path="${1:-./}" && shift
-  __docker_config_file="${1:-}" && shift
+  __docker_config_file="${1:-$__docker_context_path/Dockerfile}" && shift
+  __docker_config_dir_path="${__docker_config_file%\/Dockerfile*}"
 
   # shellcheck disable=SC2086
   if [[ "$(docker images $__docker_image_tag -q)" = "" ]]; then
@@ -102,87 +101,77 @@ run_docker_container () {
     }
   fi
   # Set options for 'docker run' command.
-  _docker_opts=()
+  __docker_opts=()
   # Interactive (Keep STDIN open even if not attached).
-  _docker_opts+=(-i)
+  __docker_opts+=(-i)
   # Allocate a pseudo-TTY.
-  _docker_opts+=(-t)
+  __docker_opts+=(-t)
   # Automatically remove the container when it exits.
-  _docker_opts+=(--rm)
+  __docker_opts+=(--rm)
   # Pass environment variables.
-  _docker_opts+=(-e "TZ=${TZ:-}")
-  _docker_opts+=(-e "AWS_ADFS_HOST=${AWS_ADFS_HOST:-}")
-  _docker_opts+=(-e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-}")
-  _docker_opts+=(-e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-}")
-  _docker_opts+=(-e "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}")
-  _docker_opts+=(-e "GITHUB_TOKEN=${GH_TOKEN:-}")
-  _docker_opts+=(-e "GH_TOKEN=${GH_TOKEN:-}")
-  _docker_opts+=(-e "NPM_TOKEN=${NPM_TOKEN:-}")
-  if [[ -r "$__runbox_repo_path/.env" ]] && [[ -f "$__runbox_repo_path/.env" ]]; then
-    _docker_opts+=(--env-file "$__runbox_repo_path/.env")
+  __env_vars=(
+    "TZ=${TZ:-}"
+    "AWS_ADFS_HOST=${AWS_ADFS_HOST:-}"
+    "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-}"
+    "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-}"
+    "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}"
+    "GITHUB_TOKEN=${GH_TOKEN:-}"
+    "GH_TOKEN=${GH_TOKEN:-}"
+    "NPM_TOKEN=${NPM_TOKEN:-}"
+  )
+  for _val in "${__env_vars[@]}"; do __docker_opts+=(-e "$_val"); done
+  unset _val
+
+  if [[ -r "$__docker_config_dir_path/.env" ]] && [[ -f "$__docker_config_dir_path/.env" ]]; then
+    __docker_opts+=(--env-file "$__docker_config_dir_path/.env")
   fi
-  # if [[ -r "$__pwd_path/.env" ]] && [[ -f "$__pwd_path/.env" ]]; then
-  #   _docker_opts+=(--env-file "$__pwd_path/.env")
-  # fi
+
   # Mount volume to map current directory (on host) to work directory (on container).
-  _docker_opts+=(-v "$__pwd_path:$__image_work_path")
+  __docker_opts+=(-v "$__pwd_path:$__image_work_path")
   # Mount volume for dotfiles to keep them synced.
-  _docker_opts+=(-v "$__docker_context_path/dotfiles:$__image_home_path/dotfiles")
+  __docker_opts+=(-v "$__docker_context_path/dotfiles:$__image_home_path/dotfiles")
+
   # Mount volumes for read-only credentials/config:
-  # Use host user's SSH keys (read-only).
-  if [[ -e "$__host_home_path/.ssh" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.ssh:$__image_home_path/.ssh:ro")
-  fi
-  # Use host user's Git config/credentials (read-only).
-  if [[ -e "$__host_home_path/.gitconfig" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.gitconfig:$__image_home_path/.gitconfig:ro")
-  fi
-  if [[ -e "$__host_home_path/.gitattributes" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.gitattributes:$__image_home_path/.gitattributes:ro")
-  fi
-  if [[ -e "$__host_home_path/.gitignore" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.gitignore:$__image_home_path/.gitignore:ro")
-  fi
-  # Use host user's Subversion configuration (read-only).
-  if [[ -e "$__host_home_path/.subversion" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.subversion:$__image_home_path/.subversion:ro")
-  fi
-  # Use host user's local runtime configuration (read-only).
-  if [[ -e "$__host_home_path/.localrc" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.localrc:$__image_home_path/.localrc:ro")
-  fi
-  # Use host user's npm runtime configuration (read-only).
-  if [[ -e "$__host_home_path/.npmrc" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.npmrc:$__image_home_path/.npmrc:ro")
-  fi
-  # Use host user's gem runtime configuration (read-only).
-  if [[ -e "$__host_home_path/.gemrc" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.gemrc:$__image_home_path/.gemrc:ro")
-  fi
-  # Use host user's Serverless runtime configuration (read-only).
-  if [[ -e "$__host_home_path/.serverlessrc" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.serverlessrc:$__image_home_path/.serverlessrc:ro")
-  fi
-  # Use host user's MongoDB runtime configuration (read-only).
-  if [[ -e "$__host_home_path/.mongorc.js" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.mongorc.js:$__image_home_path/.mongorc.js:ro")
-  fi
+  __user_conf_read_only=(
+    ".gemrc"            # Use host Ruby-gem runtime configuration (read-only).
+    ".gitattributes"    # Use host global Git settings (read-only).
+    ".gitconfig"        # Use host Git config/credentials (read-only).
+    ".gitignore"        # Use host global Git-ignore settings (read-only).
+    ".localrc"          # Use host local runtime configuration (read-only).
+    ".mongorc.js"       # Use host MongoDB runtime configuration (read-only).
+    ".npmrc"            # Use host npm runtime configuration (read-only).
+    ".serverlessrc"     # Use host Serverless runtime configuration (read-only).
+    ".ssh"              # Use host SSH keys (read-only).
+    ".subversion"       # Use host Subversion configuration (read-only).
+  )
+  for _val in "${__user_conf_read_only[@]}"; do
+    if [[ -e "$__host_home_path/$_val" ]]; then
+      __docker_opts+=(-v "$__host_home_path/$_val:$__image_home_path/$_val:ro")
+    fi
+  done
+  unset _val
+
   # Mount volumes for persistence:
-  # Allow Serverless config/prefs to be synced on host.
-  if [[ -e "$__host_home_path/.serverless" ]]; then
-    _docker_opts+=(-v "$__host_home_path/.serverless:$__image_home_path/.serverless")
-  fi
-  # Allow Travis CLI config/prefs to be synced on host.
-  _docker_opts+=(-v "$__host_home_path/.travis:$__image_home_path/.travis")
-  # Allow Sonar Lint config/prefs to be synced on host.
-  _docker_opts+=(-v "$__host_home_path/.sonarlint:$__image_home_path/.sonarlint")
-  # Allow AWS-CLI config/prefs to be synced on host.
-  _docker_opts+=(-v "$__host_home_path/.aws:$__image_home_path/.aws")
-  # Allow Terraform config/prefs to be synced on host.
-  _docker_opts+=(-v "$__host_home_path/.terraform.d:$__image_home_path/.terraform.d")
+  __user_conf_write=(
+    ".aws"              # Allow AWS-CLI config/prefs to be synced on host.
+    ".serverless"       # Allow Serverless config/prefs to be synced on host.
+    ".sonarlint"        # Allow Sonar Lint config/prefs to be synced on host.
+    ".terraform.d"      # Allow Terraform config/prefs to be synced on host.
+    ".travis"           # Allow Travis CLI config/prefs to be synced on host.
+  )
+  for _val in "${__user_conf_write[@]}"; do
+    if [[ -e "$__host_home_path/$_val" ]]; then
+      __docker_opts+=(-v "$__host_home_path/$_val:$__image_home_path/$_val")
+    fi
+  done
+  unset _val
 
-  # Container name.
-  # _docker_opts+=(--name "$__name")
+  # Make container name from image name and append datetime for uniqueness.
+  __docker_container_name="${__docker_image_tag%:*}"
+  __docker_container_name="${__docker_container_name#*\/}_$(date +%m%d%H%M)"
+  __docker_opts+=(--name "$__docker_container_name")
+  __docker_opts+=(--hostname "$__docker_container_name")
 
-  docker run "${_docker_opts[@]}" "$__docker_image_tag" "$@"
+  # Finally, run docker container.
+  docker run "${__docker_opts[@]}" "$__docker_image_tag" "$@"
 }
